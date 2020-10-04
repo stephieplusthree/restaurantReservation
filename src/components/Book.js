004b6b99dd8abe92986f61f25c5f4963c1dfac7d
stephanie <stephieplusthree@gmail.com>
2020-10-03 19:55:09 -0700

    tables added

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,6 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+    Row,
+    Col,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+    Input,
+    Button
+} from "reactstrap";
+
+import Table from "./Table";
 
 const Book = _ => {
+    const [totalTables, setTotalTables] = useState([]);
+    
+    //User's Selections
+    const [selection, setSelectrion] = useState({
+        table: {
+            name: null,
+            id: null
+        },
+        date: new Date(),
+        time: null,
+        location: "Any Location",
+        size: 0
+    });
+
+    // User's booking default
+    const [booking, setBooking] = useState({
+        name: "",
+        phone: "",
+        email: ""
+    });
+    
+    //Potential locations
+    const [locations] = useState(["Any Location", "Patio", "Inside", "Bar"])
+    const [times] = useState([
+
+    ]);
+    
     return (
         <div>
             <p>Booking Page</p>
---
+++ src/components/Table.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Book from './Book';
+
+const Table = _ => {
+    return (
+        <div>
+            <p>Table</p>
+        </div>
+    );
+}
+
+export default Book;
